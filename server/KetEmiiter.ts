79e90b11ffe995e68beb57246337159458fb9dea
Tobiloba Ajibade <tobiajibade@yandex.com>
2023-05-08 21:53:06 +0100

    little edit

---
+++ server/KetEmiiter.ts
@@ -0,0 +1,19 @@
+import { EventEmitter } from "events";
+
+/**
+ * KetEvent Class that extends the EventEmitter class
+ */
+
+class KetEvent extends EventEmitter {
+  constructor() {
+    super();
+  }
+
+  on(eventName: string | symbol, listener: (...args: any[]) => void): this {
+    
+  }
+
+  emit(eventName: string | symbol, ...args: any[]): boolean {
+    
+  }
+}
